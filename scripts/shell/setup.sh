2befb483242079b0ade5e1edb0325a24458d1c81
Jannis Schreiber <info@lietzau-consulting.de>
2022-11-01 18:14:17 +0100

    chore: gitkeep and bump api version

--- scripts/shell/setup.sh
+++ scripts/shell/setup.sh
@@ -20,22 +20,19 @@ done
 echo "npm ci"
 npm ci
 
-echo "mkdir -p force-app"
-mkdir -p force-app
-
 if [ -z "$devhubusername" ]; then
     echo "sfdx force:org:create -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -d $duration -f $configFile -a $alias -s
+    sfdx force:org:create -d "$duration" -f "$configFile" -a "$alias" -s
 else
     echo "sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s
+    sfdx force:org:create -v "$devhubusername" -d "$duration" -f "$configFile" -a "$alias" -s
 fi
 
 echo "sfdx force:source:push -u $alias"
-sfdx force:source:push -u $alias
+sfdx force:source:push -u "$alias"
 
 echo "sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias"
-sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias
+sfdx force:data:tree:import -p data/plans/standard-plan.json -u "$alias"
 
 echo "sfdx force:org:open -u $alias"
-sfdx force:org:open -u $alias
+sfdx force:org:open -u "$alias"
