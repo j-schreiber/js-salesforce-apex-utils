87441662e09229510a8e48eaae26d41864f77c0a
Jannis Schreiber <info@lietzau-consulting.de>
2023-05-20 11:10:58 +0200

    refactor: namespace problems (#5)
    - extract currency tests to unpackaged source
    - more edge cases for coverage (unpackaged)
    - remove state and country picklist dep
    - remove unused "native locale" field
    - subscriber controlled for currency fields
    - draft scratch org as UAT target

--- scripts/shell/setup.sh
+++ scripts/shell/setup.sh
@@ -21,18 +21,18 @@ echo "npm ci"
 npm ci
 
 if [ -z "$devhubusername" ]; then
-    echo "sfdx force:org:create -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -d "$duration" -f "$configFile" -a "$alias" -s
+    echo "sf force org create -d $duration -f $configFile -a $alias -s"
+    sf force org create -d "$duration" -f "$configFile" -a "$alias" -s
 else
-    echo "sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -v "$devhubusername" -d "$duration" -f "$configFile" -a "$alias" -s
+    echo "sf force org create -v $devhubusername -d $duration -f $configFile -a $alias -s"
+    sf force org create -v "$devhubusername" -d "$duration" -f "$configFile" -a "$alias" -s
 fi
 
 echo "sfdx force:source:push -u $alias"
 sfdx force:source:push -u "$alias"
 
-echo "sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias"
-sfdx force:data:tree:import -p data/plans/standard-plan.json -u "$alias"
+echo "sfdx data tree import -p data/plans/standard-plan.json -o $alias"
+sf data tree import -p data/plans/standard-plan.json -o "$alias"
 
-echo "sfdx force:org:open -u $alias"
-sfdx force:org:open -u "$alias"
+echo "sfdx org open -o $alias"
+sf org open -o "$alias"
